92f6337efab225dbc9e62eb8ba82e2e39baac944
Rahul <rahul.wa47@gmail.com>
2026-04-11 17:17:39 +0530

    added the personal info in the read only mode  with out any styles

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ const App = () => {
         email: '',
         phoneNumber: '',
         address: '',
-        zipCode: '',
         city: ''
     })
 
--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,10 +1,18 @@
 import { useState } from "react"
 
 const PersonalInfo = ({onEditClick, isEditing, onSubmit, info, inputChange}) => {
+
+  const {firstName, lastName, email, phoneNumber, address, city} = info
+
   if(!isEditing) {
     return(
       <>
-        <h1>FORM SUBMITTED</h1>
+        <h1>{firstName} {lastName}</h1>
+        <div className="personal-details">
+          <p>{phoneNumber}</p>
+          <p>{email}</p>
+          <p>{address}{address && city ? "," : ""} {city}</p>
+        </div>
         <button onClick={onEditClick}>Edit</button>
       </>
     )
@@ -17,31 +25,27 @@ const PersonalInfo = ({onEditClick, isEditing, onSubmit, info, inputChange}) => 
         <form className="personal-form" onSubmit={onSubmit}>
             <div className="field-wrapper">
               <label htmlFor="firstName">First name: </label>
-              <input onChange={inputChange} value={info.firstName} type="text"  id="firstName" name="firstName" />
+              <input onChange={inputChange} value={firstName} type="text"  id="firstName" name="firstName" required/>
             </div>
             <div className="field-wrapper">
               <label htmlFor="lastName">Last name: </label>
-              <input onChange={inputChange} value={info.lastName} type="text"  id="lastName" name="lastName"/>
+              <input onChange={inputChange} value={lastName} type="text"  id="lastName" name="lastName" required/>
             </div>
             <div className="field-wrapper">
               <label htmlFor="email">E-mail: </label>
-              <input onChange={inputChange} value={info.email} type="email" placeholder="E-mail" id="email" name="email" />
+              <input onChange={inputChange} value={email} type="email" placeholder="E-mail" id="email" name="email" required/>
             </div>
             <div className="field-wrapper">
               <label htmlFor="phoneNumber">Phone number: </label>
-              <input onChange={inputChange} value={info.phoneNumber} type="tel" id="phoneNumber" name="phoneNumber" placeholder="123-456-7890" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" inputMode="numeric"/>
+              <input onChange={inputChange} value={phoneNumber} type="tel" id="phoneNumber" name="phoneNumber" placeholder="123-456-7890" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" inputMode="numeric"/>
             </div>
             <div className="field-wrapper">
               <label htmlFor="address">Address: </label>
-              <input onChange={inputChange} value={info.address} type="text" placeholder="" id="address" name="address"/>
-            </div>
-            <div className="field-wrapper">
-              <label htmlFor="zipCode">Zip code: </label>
-              <input onChange={inputChange} value={info.zipCode} type="text" placeholder="" id="zipCode" name="zipCode" inputMode="numeric"/>
+              <input onChange={inputChange} value={address} type="text" placeholder="" id="address" name="address"/>
             </div>
             <div className="field-wrapper">
               <label htmlFor="city">City: </label>
-              <input onChange={inputChange} value={info.city} type="text" placeholder="" id="city" name="city"/>
+              <input onChange={inputChange} value={city} type="text" placeholder="" id="city" name="city"/>
             </div>
             
             <button type="submit" >Save</button>
