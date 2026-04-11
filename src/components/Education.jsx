3cfead5a30f830071a3ba181aab2c606756159de
Rahul <rahul.wa47@gmail.com>
2026-04-11 21:59:35 +0530

    added a select dropdown with years for start field

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Education = ({info, inputChange}) => {
+  const start = 1960
+  const length = 71
+  const years = Array.from({length}, (_, i) => start + i)
   return (
     <div className="education-container">
       <div className="head">
@@ -10,10 +13,14 @@ const Education = ({info, inputChange}) => {
           <input onChange={inputChange} value={info.degree} type="text"  id="degree" name="degree" required/>
         </div>
         <div className="field-wrapper">
-          
+          <label htmlFor="school">School </label>
+          <input onChange={inputChange} value={info.school} type="text"  id="school" name="school" required/>
         </div>
         <div className="field-wrapper">
-          
+          <label htmlFor="start">Start </label>
+          <select id='start' value="2026">
+            {years.map(year => <option  >{year}</option>)}
+          </select>
         </div>
       </div>
     </div>
