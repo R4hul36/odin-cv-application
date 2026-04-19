6331f546343ae8a8ad12234076c9c328ec1ceb16
Rahul <rahul.wa47@gmail.com>
2026-04-19 18:12:33 +0530

    minor styling to the fields in the personal component

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -5,11 +5,13 @@ const PersonalInfo = ({onEditClick, isEditing, onSubmit, info, inputChange}) => 
 
   const {firstName, lastName, email, phoneNumber, address, city} = info
 
+  const fullName = firstName[0].toUpperCase() + firstName.slice(1) + " " + lastName[0].toUpperCase() + lastName.slice(1)
+
   if(!isEditing) {
     return(
       <>
         <header className="header">
-          <h1>{firstName} {lastName}</h1>
+          <h1>{fullName}</h1>
           <button onClick={onEditClick}>Edit</button>
         </header>
         
@@ -28,32 +30,34 @@ const PersonalInfo = ({onEditClick, isEditing, onSubmit, info, inputChange}) => 
             <h2>Personal Details</h2>
         </div>
         <form className="personal-form" onSubmit={onSubmit}>
-            <div className="field-wrapper">
-              <label htmlFor="firstName">First name: </label>
-              <input onChange={inputChange} value={firstName} type="text"  id="firstName" name="firstName" required/>
+            <div className="full-name">
+              <div className="field-wrapper">
+                <label htmlFor="firstName">First name *</label>
+                <input onChange={inputChange} value={firstName} type="text"  id="firstName" name="firstName" required/>
+              </div>
+              <div className="field-wrapper">
+                <label htmlFor="lastName">Last name *</label>
+                <input onChange={inputChange} value={lastName} type="text"  id="lastName" name="lastName" required/>
+              </div>
             </div>
             <div className="field-wrapper">
-              <label htmlFor="lastName">Last name: </label>
-              <input onChange={inputChange} value={lastName} type="text"  id="lastName" name="lastName" required/>
-            </div>
-            <div className="field-wrapper">
-              <label htmlFor="email">E-mail: </label>
+              <label htmlFor="email">E-mail</label>
               <input onChange={inputChange} value={email} type="email" placeholder="E-mail" id="email" name="email" required/>
             </div>
             <div className="field-wrapper">
-              <label htmlFor="phoneNumber">Phone number: </label>
+              <label htmlFor="phoneNumber">Phone</label>
               <input onChange={inputChange} value={phoneNumber} type="tel" id="phoneNumber" name="phoneNumber" placeholder="123-456-7890" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" inputMode="numeric"/>
             </div>
             <div className="field-wrapper">
-              <label htmlFor="address">Address: </label>
+              <label htmlFor="address">Address</label>
               <input onChange={inputChange} value={address} type="text" placeholder="" id="address" name="address"/>
             </div>
             <div className="field-wrapper">
-              <label htmlFor="city">City: </label>
+              <label htmlFor="city">City</label>
               <input onChange={inputChange} value={city} type="text" placeholder="" id="city" name="city"/>
             </div>
             
-            <button type="submit" >Save</button>
+            <button className="submit-btn" type="submit" >Save</button>
         </form>
     </div>
   )
